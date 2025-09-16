2e797b9858959e5d170bef464ba3b762859e8732
Jino-jebarson <jinovj250@gmail.com>
2025-09-16 20:07:26 +0530

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>It's disigned by Anbumani</p>
+      <p>It's disigned by Jino Jebarson</p>
     </div>
   )
 }
