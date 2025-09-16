5ea9d32fc59608bacf6fb514317f5124bb4846ef
Jino-jebarson <jinovj250@gmail.com>
2025-09-16 20:00:41 +0530

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import { useContext } from 'react'
+import { CoinContext } from '../../context/Coincontext'
+import { Link, useNavigate } from 'react-router-dom'
+const Navbar = () => {
+  const {setCurrency} = useContext(CoinContext)
+
+  const  currencyHandler = (e)=>{
+    switch(e.target.value){
+      case "usd" :{
+        setCurrency({name:"usd",symbol:"$"})
+        break;
+      }
+      case "eur" :{
+        setCurrency({name:"eur",symbol:"€"})
+        break;
+      }
+      case "inr" :{
+        setCurrency({name:"inr",symbol:"₹" })
+        break;
+      }
+      default :{
+        setCurrency({name:"usd",symbol:"$"})
+        break;
+      }
+    }
+  }
+  return (
+    <div className='navbar'>
+      <Link to={'/'}>
+        <img src={logo} alt="" className='logo'/>
+      </Link>
+        <ul>
+          <Link to={'/'}> <li>Home</li></Link>
+            <li>Pricing</li>
+            <li>Buy/Sell</li>
+            <li>Contect</li>
+        </ul>
+        <div className="nav-right">
+            <select onChange={currencyHandler} >
+                <option value="usd">USD</option>
+                <option value="eur">EUR</option>
+                <option value="inr">INR</option>
+            </select>
+            <button>Sign up</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const SignUp = () => {
+  return (
+    <div>
+        <h1>SigUp</h1>
+    </div>
+  )
+}
+
+export default SignUp
